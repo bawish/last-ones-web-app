0279e74623d2998148ddb8190052872e2635c4a9
bawish <barretts@gmail.com>
2013-10-04 15:57:39 -0400

    added a couple fake posts

--- app/views.py
+++ app/views.py
@@ -5,6 +5,17 @@ from app import app
 @app.route('/index')
 def index():
 	user = { 'nickname': 'Barrett' } #fake user
+	posts = [ # fake array of posts
+		{
+			'author': { 'nickname': 'John'},
+			'body': 'Beautiful day in portland!'
+		},
+		{
+			'author': { 'nickname': 'Susan'},
+			'body': 'The Avengers movie sucked'
+		}
+	]
 	return render_template("index.html",
 		title = 'Home',
-		user = user)
+		user = user,
+		posts = posts)
