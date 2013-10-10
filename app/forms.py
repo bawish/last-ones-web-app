1e234af812e45f007d2caf961bf3febc0804b4bf
bawish <barretts@gmail.com>
2013-10-10 16:00:59 -0400

    added 'flash' message once user inputs data on form page

--- app/forms.py
+++ app/forms.py
@@ -2,10 +2,11 @@ from flask_wtf import Form
 from wtforms import TextField, BooleanField
 from wtforms.validators import DataRequired
 
-# IMPORTS FROM TUTORIAL DON'T WORK (CUZ OF DIFFERENT VERSION?)
+# IMPORTS FROM TUTORIAL DON'T WORK (CUZ OF DIFFERENT VERSION)
 # from flask.ext.wtf import Form, TextField, BooleanField
 # from flask.ext.wtf import Required
 
+# we import this class (and the form variables) in views.py
 class LoginForm(Form):
 	openid = TextField('openid', validators = [DataRequired()])
 	remember_me = BooleanField('remember_me', default = False)
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from forms import LoginForm
 
+# this is the method for rendering the index page
 @app.route('/')
 @app.route('/index')
 def index():
@@ -21,9 +22,18 @@ def index():
 		user = user,
 		posts = posts)
 
+# method for rendering the log in page
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+	
+	# initiate the form object
 	form = LoginForm()
+	
+	# here is where we validate and act on entered form data
+	if form.validate_on_submit():
+		flash("Login requested for OpenID='" + form.openid.data + "', \
+			remember_me=" + str(form.remember_me.data))
+		return redirect('/index')
 	return render_template('login.html',
 		title = 'Sign In',
 		form = form)
